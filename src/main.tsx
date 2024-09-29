a1b7d928b9f1b610a1d05bf50a04cafd84f51e01
Haziq Suhaimi <muhammad.haziq@pixlr.com>
2024-09-29 08:36:07 +0800

    added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: import.meta.env.BASE_URL,
+    element: <App />,
+    // errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
